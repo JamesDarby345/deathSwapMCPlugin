68728ffacebb6b8c80808c58293e942cf896ea6d
James.D <49734270+JamesDarby345@users.noreply.github.com>
2020-05-10 20:41:29 -0700

    comments

--- threePplDeathSwap/Main.java
+++ threePplDeathSwap/Main.java
@@ -7,7 +7,10 @@ import threePplDeathSwap.commands.fastSwap;
 import threePplDeathSwap.commands.deathListener.deathListener;
 
 public class Main extends JavaPlugin {
-	public static boolean freeze;
+	
+	public static boolean freeze;//used to freeze players in listeners and toggled in deathSwapRunnable
+	
+	//called on server start, intilizes the commands 
 	public void onEnable() {
 		freeze = false;
 		saveDefaultConfig();
