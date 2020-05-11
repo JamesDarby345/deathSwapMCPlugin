6e4fac2dde0b2bed8b16061fdad01adc784a8d12
James.D <49734270+JamesDarby345@users.noreply.github.com>
2020-05-10 18:20:18 -0700

    copy

--- utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package threePplDeathSwap.commands.utils;
-
-import org.bukkit.ChatColor;
-
-public class Utils {
-	public static String chat (String s) {
-		return ChatColor.translateAlternateColorCodes('&', s);
-	}
-}
